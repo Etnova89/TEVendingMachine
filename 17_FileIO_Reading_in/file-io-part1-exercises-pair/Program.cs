2bd33d8ddf8bf88081eb51017525c02759d44e11
Eric Terranova <terranova0689@gmail.com>
2019-02-04 13:56:56 -0500

    finished Alice Search

--- 17_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
+++ 17_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace file_io_part1_exercises_pair
 {
@@ -6,7 +8,73 @@ namespace file_io_part1_exercises_pair
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Please enter file path.");
+            string filePath = Console.ReadLine();
+
+            Console.WriteLine("Please enter file name");
+            string fileName = Console.ReadLine();
+
+            string fullPath = Path.Combine(filePath, fileName);
+
+            List<string> allWords = new List<string>();
+            List<string> allSentences = new List<string>();
+            int sentenceCount = 0;
+            try
+            {
+                using (StreamReader reader = new StreamReader(fullPath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+
+                        if (line != "")
+                        {
+                            string[] lineArray = line.Split(' ');
+                            allWords.AddRange(lineArray);
+                        }
+                    }
+
+                }
+
+                using (StreamReader readerSentences = new StreamReader(fullPath))
+                {
+                    while (!readerSentences.EndOfStream)
+                    {
+
+                        string sentence = readerSentences.ReadLine();
+                        foreach (char item in sentence)
+                        {
+                            if (item == '!')
+                            {
+                                sentenceCount++;
+                            }
+                            else if (item == '.')
+                            {
+                                sentenceCount++;
+                            }
+                            else if (item == '?')
+                            {
+                                sentenceCount++;
+                            }
+
+                        }
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error reading the file");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(allWords.Count);
+            //Console.WriteLine(allSentences.Count);
+            Console.WriteLine(sentenceCount);
+
+            Console.ReadLine();
         }
     }
 }
