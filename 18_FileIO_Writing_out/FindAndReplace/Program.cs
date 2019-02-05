f4a6b557032a0ccd8573d1d673835b767481012c
Eric Terranova <terranova0689@gmail.com>
2019-02-05 13:53:16 -0500

    finished 18 file writing

--- 18_FileIO_Writing_out/FindAndReplace/Program.cs
+++ 18_FileIO_Writing_out/FindAndReplace/Program.cs
@@ -28,17 +28,24 @@ namespace FindAndReplace
                 }
             }
 
-            Console.WriteLine("Please enter the destination for the copy: ");               
+            Console.WriteLine("Please enter the name for the copy: ");
             string returnPath = Console.ReadLine();
 
             string returnFullPath = Path.Combine(filePath, returnPath);
+
+            if (File.Exists(returnFullPath) == true)
+            {
+                Console.WriteLine("This File name already exists");
+                Console.ReadLine()
+                System.Environment.Exit(0);
+            }
 
 
             Console.WriteLine("Please enter your search phrase: ");
             string searchPhrase = Console.ReadLine();
             Console.WriteLine("What would you like the search phrase to be replaced with? ");
             string replacementPhrase = Console.ReadLine();
-
+            int counter = 0;
             try
             {
                 using (StreamReader sr = new StreamReader(fullPath))
@@ -48,9 +55,17 @@ namespace FindAndReplace
                         while (!sr.EndOfStream)
                         {
                             string line = sr.ReadLine();
+                            if (line.Contains(searchPhrase))
+                            {
+                                counter++;
+                            }
+
                             line = line.Replace(searchPhrase, replacementPhrase);
                             sw.WriteLine(line);
+
                         }
+                        Console.WriteLine($"There have been {counter} changes");
+                        Console.ReadLine();
                     }
                 }
             }
