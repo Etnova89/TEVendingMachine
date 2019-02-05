b581e38a21eaf3786b07311f4a136f38100592cd
Patrick Williams <pwilliams0802@gmail.com>
2019-02-05 12:49:22 -0500

    Pre-emptive commit before testing

--- 18_FileIO_Writing_out/FindAndReplace/Program.cs
+++ 18_FileIO_Writing_out/FindAndReplace/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FindAndReplace
 {
@@ -6,7 +7,35 @@ namespace FindAndReplace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the file path: ");
+            string filePath = Console.ReadLine();
+            Console.WriteLine("Please enter the file name: ");
+            string fileName = Console.ReadLine();
+            Console.WriteLine("Please enter the return file path: ");
+            string returnPath = Console.ReadLine();
+            string fullPath = Path.Combine(filePath, fileName);
+            string returnFullPath = Path.Combine(filePath, returnPath);
+
+
+            Console.WriteLine("Please enter your search phrase: ");
+            string searchPhrase = Console.ReadLine();
+            Console.WriteLine("What would you like the search phrase to be replaced with? ");
+            string replacementPhrase = Console.ReadLine();
+
+            using (StreamReader sr = new StreamReader(fullPath))
+            {
+                using (StreamWriter sw = new StreamWriter(returnFullPath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        line = line.Replace(searchPhrase, replacementPhrase);
+                        sw.WriteLine(line);
+                    }
+                }
+            }
         }
+
     }
 }
+
