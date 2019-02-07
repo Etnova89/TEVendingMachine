0c6c17597d8792d1b15117f3d3d720f7cf9f6bb1
Eric Terranova <terranova0689@gmail.com>
2019-02-07 12:10:23 -0500

    first tests, VMItem properties

--- Mini-Capstone/Capstone/Classes/VendingMachineItem.cs
+++ Mini-Capstone/Capstone/Classes/VendingMachineItem.cs
@@ -6,6 +6,10 @@ namespace Capstone.Classes
 {
     public class VendingMachineItem
     {
+        public string Slot { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+
 
     }
 }
--- Mini-Capstone/CapstoneTests/UnitTest1.cs
+++ Mini-Capstone/CapstoneTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+using System.Collections.Generic;
 
 namespace CapstoneTests
 {
@@ -6,8 +8,50 @@ namespace CapstoneTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void VendingMachineCreate()
         {
+            VendingMachine vm = new VendingMachine();
+            Assert.IsNotNull(vm);
         }
+
+        [TestMethod]
+        public void VendingMachineCreateItemList()
+        {
+            List<VendingMachineItem> items = new List<VendingMachineItem>();
+            Assert.IsNotNull(items);
+        }
+
+        [TestMethod]
+        public void VendingMachineAddToItemList()
+        {
+            List<VendingMachineItem> items = new List<VendingMachineItem>();
+            VendingMachineItem item = new VendingMachineItem()
+            {
+                Slot = "a1",
+                ProductName = "item",
+                Price = 1.10m,
+            };
+            items.Add(item);
+            Assert.AreEqual(1, items.Count);
+        }
+
+        [TestMethod]
+        public void VendingMachineItemAdd()
+        {
+            VendingMachine vm = new VendingMachine();
+            int oldCount = vm.ToArray().Length;
+            VendingMachineItem item = new VendingMachineItem()
+            {
+                Slot = "a1",
+                ProductName = "item",
+                Price = 1.10m,
+            };
+            vm.Add(item);
+            int newCount = vm.ToArray().Length;
+            Assert.AreEqual(oldCount + 1, newCount);
+
+
+        }
+
     }
 }
